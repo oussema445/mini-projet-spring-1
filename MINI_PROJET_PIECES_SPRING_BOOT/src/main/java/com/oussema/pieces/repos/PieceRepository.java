48260b2a7442d53d5956178c218e67dc2ffee8fd
oussema Mhamed <120645606+oussema445@users.noreply.github.com>
2024-12-03 14:03:53 +0100

    ok

--- MINI_PROJET_PIECES_SPRING_BOOT/src/main/java/com/oussema/pieces/repos/PieceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oussema.pieces.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.oussema.pieces.entities.Piece;
-
-public interface PieceRepository extends JpaRepository<Piece, Long> {
-
-}
